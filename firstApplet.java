7c30b16b469ce1b3317a36bc68d65544d7a95875
wzs6666 <mr_wangzs@163.com>
2021-04-30 00:13:19 +0800

    aaa

---
+++ firstApplet.java
@@ -0,0 +1,24 @@
+import java.applet.*;
+import java.awt.*;
+
+public class firstApplet extends Applet {
+    public String s;
+    public void init() {
+        s = new String("applet");
+    }
+    public void paint(Graphics g) {
+        g.drawString(s, 25,25);
+    }
+}
+
+class FreshJuice {
+   enum FreshJuiceSize{ SMALL, MEDIUM , LARGE }
+   FreshJuiceSize size;
+}
+ 
+public class FreshJuiceTest {
+   public static void main(String[] args){
+      FreshJuice juice = new FreshJuice();
+      juice.size = FreshJuice.FreshJuiceSize.MEDIUM  ;
+   }
+}
